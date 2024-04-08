105ddd3b5608c77a552bff5d187ab2560b210462
Gettis <fedyuncev1133f5@gmail.com>
2024-04-08 13:25:24 +0300

    add images, fonts, modal plugin, gallery plugin

--- src/scripts/index.mjs
+++ src/scripts/index.mjs
@@ -1,1 +1,25 @@
-console.log(123);
+// swiper
+// var swiper = new Swiper(".mySwiper", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//         rotate: 50,
+//         stretch: 0,
+//         depth: 100,
+//         modifier: 1,
+//         slideShadows: true,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+// });
+
+const Modal = new HystModal({
+    linkAttributeName: "data-hystmodal",
+});
+
+lightGallery(document.getElementById('animated-thumbnails-gallery'), {
+    thumbnail: true,
+});
